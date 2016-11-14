56a3ba10da6d695014c023459d89f78b354e355f
wurongqiu <wurongqiu@gmail.com>
2016-11-14 18:14:27 +0800

    add monitor module

---
+++ app/src/main/java/com/wurq/dex/appmonitor/monitorApplication.java
@@ -0,0 +1,14 @@
+package com.wurq.dex.appmonitor;
+
+import android.app.Application;
+
+/**
+ * Created by wurongqiu on 16/11/14.
+ */
+public class monitorApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
+
